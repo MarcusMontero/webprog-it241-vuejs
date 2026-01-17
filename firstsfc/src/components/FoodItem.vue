92264b1fbe6aac8eff8ad14acc3325219dbfd728
MarcusMontero <mumontero@student.apc.edu.ph>
2026-01-17 17:33:01 +0800

    Update FoodItem.vue

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -11,7 +11,7 @@ export default {
     data() {
         return {
             name: 'Potato',
-            message: 'I love Potato',
+            message: 'I love Potatos',
             clicks: 0
         }
     },

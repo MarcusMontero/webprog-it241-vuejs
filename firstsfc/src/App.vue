1a6a87d9637374c43848c96e3e2ac55628999547
MarcusMontero <mumontero@student.apc.edu.ph>
2026-01-17 17:37:33 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,22 +1,9 @@
 <template>
-  <h1>Food</h1>
-  <food-item />
-  <food-item />
-  <food-item />
+  <h1>Personal Profile and Fruits</h1>
+  <food-item/>
+  <food-item2/>
 </template>
 
 <script></script>
 
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    width: 120px;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-  #app > div:hover {
-    cursor: pointer;
-  }
-</style>                  
+<style></style>

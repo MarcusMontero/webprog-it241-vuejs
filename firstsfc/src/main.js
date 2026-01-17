5059cdf2c7983b60fd0f12bf757b03ecf1da2f23
MarcusMontero <mumontero@student.apc.edu.ph>
2026-01-17 18:29:25 +0800

    Update main.js

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -5,29 +5,10 @@ import PersonalProfile from './components/PersonalProfile.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 
-
 const app = createApp(App)
+
 app.component('personal-profile', PersonalProfile)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
-app.mount('#app')
-
-app.mixin({
-  mounted() {
-    const links = document.querySelectorAll("nav ul li a[href^='#']")
-
-    links.forEach(link => {
-      link.addEventListener("click", function (e) {
-        e.preventDefault()
-        const target = document.querySelector(this.getAttribute("href"))
-        if (target) {
-          target.scrollIntoView({ behavior: "smooth" })
-        }
-      })
-    })
-
-    console.log("Personal profile website loaded")
-  }
-})
 
 app.mount('#app')

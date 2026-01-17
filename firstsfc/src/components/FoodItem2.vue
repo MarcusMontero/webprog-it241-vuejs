d5e5b4868321b8536d6332b9513c0490b5b33260
MarcusMontero <mumontero@student.apc.edu.ph>
2026-01-17 17:37:03 +0800

    Update FoodItem2.vue

--- firstsfc/src/components/FoodItem2.vue
+++ firstsfc/src/components/FoodItem2.vue
@@ -1,1 +1,29 @@
+<template>
+  <div class="Potato">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
 
+<script>
+export default {
+  data() {
+    return {
+      name: 'Potato',
+      message: 'I love Potato'
+    }
+  }
+};
+</script>
+
+<style>
+  .durian {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: rgb(213, 237, 76);
+  }
+
+
+</style>
